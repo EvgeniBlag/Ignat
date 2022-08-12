850a078f77afae6745d59e1a322399d3ca721a8a
EvgeniBlag <blagodarni@gmail.com>
2022-08-12 12:41:22 +0200

    ignat2

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -10,24 +10,16 @@ export type MessagePropsType = {
 
 function Message(props: MessagePropsType) {
     return (
-
         <div className={s.message}>
 
             <img className={s.avatar} src={props.avatar} alt={"avatar"} />
-
-            <div className={s.angle}>
-
-                <div className={s.content}>
-
-                    <div className={s.name}>{props.name}</div>
-                    <div className={s.text}>{props.message}</div>
-                    <div className={s.time}>{props.time}</div>
-                </div>
-
-
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
             </div>
+            <div className={s.angle}></div>
         </div>
-
     )
 }
 

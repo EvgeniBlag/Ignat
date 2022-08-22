b7b6ce118afa136fd0fbf73dd887577f2b94908e
EvgeniBlag <blagodarni@gmail.com>
2022-08-22 11:55:02 +0200

    test

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -17,16 +17,19 @@ beforeEach(() => {
 test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
-    console.log(newState)
-    // expect(...).toBe(...)
+    
+     expect(newState.length).toBe(6)
+     expect(newState [0].name).toBe('Александр')
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
-
+    expect(newState.length).toBe(6)
+    expect(newState [0].name).toBe('Кот')
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
-
+    expect(newState.length).toBe(4)
+    expect(newState [1].name).toBe('Виктор')
 
 })
